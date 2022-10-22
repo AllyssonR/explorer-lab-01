e70b84d40dd07244a646c61338e73df50c13e89f
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2022-10-22 14:28:33 -0300

    import and apply imask

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./css/index.css"
-
+import IMask from "imask"
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
@@ -14,5 +14,41 @@ function setCardType(type) {
   ccBgColor02.setAttribute("fill", colors[type][1])
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
+setCardType("visa")
 
-setCardType("mastercard")
+const cardNumber = document.querySelector("#card-number")
+const expirationDate = document.querySelector("#expiration-date")
+const securityCode = document.querySelector("#security-code")
+/*const cardHolder = document.querySelector("#card-holder")*/
+
+const cardNumberPattern = {
+  mask: "0000 0000 0000 0000",
+}
+
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+const securityCodePattern = {
+  mask: "0000",
+}
+/*const cardHolderPattern = {
+  mask: "",
+  maxLength: 20,
+}*/
+
+const cardNumberMaked = IMask(cardNumber, cardNumberPattern)
+const expirationDateMaked = IMask(expirationDate, expirationDatePattern)
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+//const cardHolderMasked = IMask(cardHolder, cardHolderPattern)
